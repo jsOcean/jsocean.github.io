7dc52c7eef4f03ba67ff37dfcde4f7334ace4798
jsocean <997659148@qq.com>
2020-05-22 11:01:19 +0800

    Site updated: 2020-05-22 11:01:16

--- js/top.js
+++ js/top.js
@@ -1,5 +1,5 @@
 /*Scroll to top when arrow up clicked*/
-$(window).scroll(function() {
+$(window).scroll(function () {
     var height = $(window).scrollTop();
     if (height > 300) {
         $('#back2Top').fadeIn(500);
@@ -7,8 +7,8 @@ $(window).scroll(function() {
         $('#back2Top').fadeOut(500);
     }
 });
-$(document).ready(function() {
-    $("#back2Top").click(function(event) {
+$(document).ready(function () {
+    $("#back2Top").click(function (event) {
         event.preventDefault();
         $("html, body").animate({ scrollTop: 0 }, "slow");
         return false;
